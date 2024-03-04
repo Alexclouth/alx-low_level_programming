8d71da7455974988816bf732d7f6c29d8ff1cdfc
AlClouth <alphilo@example.com>
2024-03-04 03:46:10 -0800

    done

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,23 +1,19 @@
-#include "main.h"
+#include "holberton.h"
 /**
- *_memset -   fills memory with a constant byte
- *@s: pointer block of memory to fill
- *@b: constant byte
- *@n: bytes of the memory
- *Return: dest
+ * _memset - function that fills memory with a constant byte
+ *@s: first value
+ *@b: second value
+ *@n: thirth value
+ *
+ * Return: char with result of memset
  */
-
 char *_memset(char *s, char b, unsigned int n)
-
 {
 	unsigned int i;
 
-
 	for (i = 0; i < n; i++)
 	{
-		*(s + i) = b; /*add 1 position s*/
-
+		s[i] = b;
 	}
-
 	return (s);
 }
